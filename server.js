700ba0835750512eedfbafcdd7f4599eaadee7c1
VyNas07 <vyktorf07@gmail.com>
2024-07-26 04:44:38 -0300

    update

--- server.js
+++ server.js
@@ -2,3 +2,20 @@
 const express = require('express');
 const app = express();
 
+//importar o módulo de rotas
+
+//importar o módulo de validação
+
+//importar o módulo de autenticação
+
+//importar o módulo de middlewares
+
+//importar o módulo de configuração
+
+//importar o módulo de banco de dados
+
+//importar o módulo de log
+
+//importar o módulo de envio de email
+
+//importar o módulo de envio de push
